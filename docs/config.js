8a1ea46fbda321e6af2b623398551692d25e2427
Akash <36806314+agupt013@users.noreply.github.com>
2025-08-24 02:33:41 -0700

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -4,6 +4,8 @@
     TIME_DISPLAY_MODE: (params.get('time') || 'relative'), // relative | absolute
     DISPLAY_TIMEZONE: (params.get('tz') || 'local'), // local | UTC
     ABSOLUTE_FORMAT: 'yyyy-MM-dd HH:mm:ss',
-    NUMBER_PRECISION: Number(params.get('precision') || 3)
+    NUMBER_PRECISION: Number(params.get('precision') || 3),
+    HOMEPAGE_URL: (params.get('home') || 'https://akashagupta.com/'),
+    ENABLE_VISITOR_COUNTER: (params.get('vc') || 'true') !== 'false'
   };
 })();
